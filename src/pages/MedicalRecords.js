76990539f9d908eef6d1520476faa90261e78f4b
sergejsoldat20 <sergej.s.soldat20@gmail.com>
2023-02-05 18:38:31 +0100

    dodano u procesu rada i svi nalazi

--- src/pages/MedicalRecords.js
+++ src/pages/MedicalRecords.js
@@ -1,32 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { Grid, List } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import { Box } from "@mui/system";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
-import TableContainer from "@mui/material/TableContainer";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import { Button, Row, Col, message } from "antd";
-import { useNavigate } from "react-router-dom";
-import CheckIfNurse from "../utils/CheckIfNurse";
+import { Grid, List, Button } from "@mui/material";
 import ViewMedicalRecord from "./ViewMedicalRecord";
 import recordsService from "../services/recordsService.service";
 export default function medicalRecords() {
-  // const navigate = useNavigate();
-  const [expanded, setExpanded] = useState([]);
   const [medicalRecords, setMedicalRecords] = useState([]);
+  const [displayAllRecords, setDisplayAllRecords] = useState(true);
 
   useEffect(() => {
     loadMedicalRecords();
-  });
+  }, [medicalRecords]);
 
   const loadMedicalRecords = async () => {
     recordsService
@@ -43,10 +26,14 @@ export default function medicalRecords() {
       });
   };
 
-  const valid = (x) => {
-    if (x === "true") return "validan";
-    else return "nije validan";
+  const handleFilterInvalid = () => {
+    setDisplayAllRecords(false);
   };
+
+  const handleShowAll = () => {
+    setDisplayAllRecords(true);
+  };
+
   return (
     <Grid
       container
@@ -56,14 +43,28 @@ export default function medicalRecords() {
       justifyContent="center"
     >
       <h3>Lista rezultata</h3>
+      <div>
+        <Button onClick={handleFilterInvalid}>U procesu rada</Button>
+        <Button onClick={handleShowAll}>Svi</Button>
+      </div>
       <List>
-        {medicalRecords.map((medicalRecord, index) => (
-          <ViewMedicalRecord
-            id={medicalRecord.id}
-            key={index}
-            sx={{ width: 1200 }}
-          />
-        ))}
+        {displayAllRecords
+          ? medicalRecords.map((medicalRecord, index) => (
+              <ViewMedicalRecord
+                id={medicalRecord.id}
+                key={index}
+                sx={{ width: 1200 }}
+              />
+            ))
+          : medicalRecords
+              .filter((medicalRecord) => medicalRecord.isValid === "false")
+              .map((medicalRecord, index) => (
+                <ViewMedicalRecord
+                  id={medicalRecord.id}
+                  key={index}
+                  sx={{ width: 1200 }}
+                />
+              ))}
       </List>
     </Grid>
   );
